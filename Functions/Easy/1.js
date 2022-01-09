630c40b8450181c582b08404db2d57a1fc8b7aca
Omkar-Ghate <omkarghate01@gmail.com>
2022-01-09 21:42:30 +0530

    added question 1

---
+++ Functions/Easy/1.js
@@ -0,0 +1,7 @@
+// Given a and b, your function should return the value of ab
+// Example:
+// Input: power(2,3) ––> Output: 8
+
+const findPow = (a, b) => Math.pow(a, b);
+
+console.log(findPow(2, 5));
