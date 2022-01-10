d2eaad146cfdf6472a1270a0a6d9baf75a27b842
Omkar-Ghate <omkarghate01@gmail.com>
2022-01-10 11:13:53 +0530

    add question 3

---
+++ Functions/Medium/3.js
@@ -0,0 +1,15 @@
+/*
+Given an array and two numbers, your function should replace all entries of first number in an array with the second number.
+Example:
+Input: replace([1,5,3,5,6,8], 5, 10) ––> Output: [1,10,3,10,6,8]
+*/
+
+const replaceAll = (array, num1, num2) => {
+    for(let i=0 ; i<array.length ; i++){
+        if(array[i] === num1)array[i] = num2;
+    }
+
+    return array;
+}
+
+console.log(replaceAll([1,5,3,5,6,8], 5, 10));
